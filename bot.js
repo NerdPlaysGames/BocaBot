f56c8f86b311eaefa532fa6b33a5ca3aba68aabe
NerdPlaysGames <32402452+NerdPlaysGames@users.noreply.github.com>
2021-10-18 19:17:20 -0400

    Added recurring status and slash-command

--- bot.js
+++ bot.js
@@ -59,7 +59,7 @@ ioClient.on('connect_error', (err) => {
 cron.schedule('0 6 * * *', closureUpdate);
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_WEBHOOKS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] });
 
 // Fires once when the bot is ready
 client.on('ready', () => {
@@ -70,6 +70,69 @@ client.on('ready', () => {
   notamChannel = guild.channels.cache.get(process.env.notams);
 });
 
+<<<<<<< HEAD
+client.on('interactionCreate', async (interaction) => {
+  if (interaction.commandName === 'closures' && interaction.options._subcommand === 'listed') {
+    const response = await fetch('https://status.nerdpg.live/api/roadClosures');
+    const data = await response.json();
+
+    let embed = new MessageEmbed();
+    embed.setTitle('All listed closures');
+
+    if (data.length > 0) {
+      for (let i = 0; i < data.length; i++) {
+        let closure = data[i];
+
+        let startTime = closure.time.split(' to ')[0].replace('.', '');
+        let closureDateStart = new Date(`${closure.date} ${startTime} GMT-0500`);
+
+        let endTime = closure.time.split(' to ')[1].replace('.', '');
+        let closureDateEnd = new Date(`${closure.date} ${endTime} GMT-0500`);
+
+        if (endTime.toUpperCase().includes('12:00 AM')) {
+          closureDateEnd.setDate(closureDateEnd.getDate() + 1);
+        }
+
+        embed.addField(`${closure.type} - ${closure.status}`, `Starts:<t:${closureDateStart.getTime() / 1000}:R>\nEnds: <t:${closureDateEnd.getTime() / 1000}:R>`);
+      }
+    } else {
+      embed.setDescription('There are no closures listed :\'(');
+    }
+    interaction.reply({ embeds: [embed] });
+  }
+
+  if (interaction.commandName === 'closures' && interaction.options._subcommand === 'today') {
+    const response = await fetch('https://status.nerdpg.live/api/roadClosures/today');
+    const data = await response.json();
+
+    let embed = new MessageEmbed();
+    embed.setTitle('Today\'s listed closures');
+
+    if (data.length > 0) {
+      for (let i = 0; i < data.length; i++) {
+        let closure = data[i];
+
+        let startTime = closure.time.split(' to ')[0].replace('.', '');
+        let closureDateStart = new Date(`${closure.date} ${startTime} GMT-0500`);
+
+        let endTime = closure.time.split(' to ')[1].replace('.', '');
+        let closureDateEnd = new Date(`${closure.date} ${endTime} GMT-0500`);
+
+        if (endTime.toUpperCase().includes('12:00 AM')) {
+          closureDateEnd.setDate(closureDateEnd.getDate() + 1);
+        }
+
+        embed.addField(`${closure.type} - ${closure.status}`, `Starts:<t:${closureDateStart.getTime() / 1000}:R>\nEnds: <t:${closureDateEnd.getTime() / 1000}:R>`);
+      }
+    } else {
+      embed.setDescription('There are no closures today :\'(');
+    }
+    interaction.reply({ embeds: [embed] });
+  }
+});
+
+=======
+>>>>>>> 51f176c79e02d5c9d16d27676774a06dd4f8cc9f
 async function closureUpdate() {
   const response = await fetch('https://status.nerdpg.live/api/roadClosures/today');
   const data = await response.json();
